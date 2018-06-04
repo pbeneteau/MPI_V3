82674a045a5af2d3702ed7ae50421c4b9a621c8a
Paul <paulbeneteau@hotmail.fr>
2018-06-04 15:39:37 +0200

    jtyty

---
+++ MPI/Automate.h
@@ -0,0 +1,57 @@
+#ifndef MPI_AUTOMATE_H
+#define MPI_AUTOMATE_H
+
+#include "Etat.h"
+using namespace std;
+#include <string>
+#include <vector>
+
+
+class Automate
+{
+    
+    private :
+    int             _nb_symboles;
+    int             _nb_etats;
+    int             _nb_etats_initiaux;
+    int             _nb_etats_finaux;
+    int             _nb_transitions;
+    
+    std::vector<Etat*> _Etats;
+    std::vector<Etat*> _Etats_initiaux;
+    std::vector<Etat*> _Etats_terminaux;
+    public :
+    void            lecture_fichier(char *chemin);
+    void            afficher();
+    /*
+     **  GETTEURS & SETTEURS
+     */
+    Automate();
+    Automate(int nb_etats);
+    void            set_nb_symboles(std::string ligne);
+    void            set_nb_etats(string ligne);
+    void            set_nb_transitions(string ligne);
+    void            set_etats(string ligne);
+    void            set_etat(Etat *new_Etat);
+    void            set_etats_initiaux(string ligne);
+    void            set_etats_terminaux(string ligne);
+    void            set_transitions(string ligne);
+    int             get_nb_etats();
+    Etat*           get_etat(int id);
+    Etat*           get_etat_determinsite(string nom_deterministe);
+    void            set_transition_etat(int id, Etat *source, char mid, Etat *dest);
+    bool            is_unique(string comp);
+    void            remove_doublon(string &content);
+    void            update_deterministe(vector<Etat*> old_terminaux);
+    /*
+     **  ANALYSE
+     */
+    bool est_asynchrone();
+    bool            est_un_automate_deterministe();
+    bool            est_un_automate_complet();
+    void            completion();
+    void            determinisation_et_completion_automate_synchrone();
+    Automate        determinisation_et_completion_automate_asynchrone();
+};
+
+#endif //MPI_Automate_H
---
+++ MPI/Etat.h
@@ -0,0 +1,35 @@
+//
+// Created by anton on 14/05/2018.
+//
+
+#ifndef MPI_ETAT_H
+#define MPI_ETAT_H
+#include <vector>
+#include <tuple>
+
+
+
+class Etat
+{
+private :
+    int _id;
+    std::string _nom_determinisation;
+    std::vector<std::tuple<Etat*, char, Etat*>> _transitions;
+
+public :
+    
+    Etat(int _id);
+    int get_id();
+    void set_id(int id);
+
+    void increment_transition();
+    void afficher();
+    void set_transition(Etat* source, const char *mid, Etat* dest);
+    void set_nom_determinisation(std::string nom_etat);
+    std::string get_nom_determinisation();
+    std::vector<std::tuple<Etat*, char, Etat*>> get_transition();
+
+};
+typedef std::vector<std::tuple<Etat*,  char, Etat*>> t_tr;
+
+#endif //MPI_ETAT_H
