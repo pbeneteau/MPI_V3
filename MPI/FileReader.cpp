f74c795c297bfe0076c5b642acfacd090f1a4a54
Maxence Guegnolle Santi <maxence@MacBook-Pro-de-Maxence.local>
2018-06-04 15:40:14 +0200

    oui

---
+++ MPI/FileReader.cpp
@@ -0,0 +1,95 @@
+//
+//  FileReader.cpp
+//  MPI
+//
+//  Created by Maxence on 04/06/2018.
+//  Copyright © 2018 Maxence. All rights reserved.
+//
+
+#include <fstream>
+#include <iostream>
+#include <cstdlib>
+#include "FileReader.hpp"
+#include "Automate.h"
+
+using namespace std;
+
+void    Automate::lecture_file(char *chemin)
+{
+	string      line;
+	
+	ifstream file(chemin, ios::in); // Ouverture du fichier en lecture
+	if (!file)
+	{
+		cout << "Erreur lors de la du file !" << endl;
+		file.close();
+		exit(1);
+	}
+	cout << "Lecture du fichier " << chemin << " en cours !" << endl;
+	/*
+	 **  Definiton des symboles
+	 */
+	if (!getline(file, line))
+	{
+		cout << "Erreur lors de la lecture du fichier ! " << endl;
+		exit(1);
+	}
+	set_nb_symboles(line);
+	if (_nb_symboles < 1 || _nb_symboles > 26)
+	{
+		cout << "Erreur dans le nombre de symboles" << endl;
+		exit(1);
+	}
+	/*
+	 **  Definiton des etats
+	 */
+	if (!getline(file, line))
+	{
+		cout << "Erreur lors de la lecture du fichier ! " << endl;
+		exit(1);
+	}
+	set_etats(line);
+	if (_nb_etats == 0)
+	{
+		cout << "0 etats dans le fichier" << endl;
+		exit(1);
+	}
+	/*
+	 **  Definiton des etats initiaux
+	 */
+	if (getline(file, line) && line.find_first_not_of("0123456789") != string::npos)
+		set_etats_initiaux(line);
+	else
+	{
+		cout << "Pas d'etat initiaux" << endl;
+		exit(1);
+	}
+	/*
+	 **  Definiton des etats terminaux
+	 */
+	if (getline(file, line) && line.find_first_not_of("0123456789") != string::npos)
+		set_etats_terminaux(line);
+	else
+	{
+		cout << "Pas d'etat initiaux" << endl;
+		exit(1);
+	}
+	/*
+	 **  Definiton du nombre de transitions
+	 */
+	if (!getline(file, line))
+	{
+		cout << "Erreur lors de la lecture du file ! " << endl;
+		exit(1);
+	}
+	set_nb_transitions(line);
+	if (_nb_transitions == 0)
+	{
+		cout << "0 transitions dans le file" << endl;
+		exit(1);
+	}
+	while (getline(file, line))
+	{
+		set_transitions(line);
+	}
+}
---
+++ MPI/FileReader.hpp
@@ -0,0 +1,14 @@
+//
+//  FileReader.hpp
+//  MPI
+//
+//  Created by Maxence on 04/06/2018.
+//  Copyright © 2018 Maxence. All rights reserved.
+//
+
+#ifndef FileReader_hpp
+#define FileReader_hpp
+
+#include <stdio.h>
+
+#endif /* FileReader_hpp */
