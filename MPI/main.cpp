962f18b647500351239927a532444ad3d8522e8c
Maxence Guegnolle Santi <maxence@MacBook-Pro-de-Maxence.local>
2018-06-04 15:00:18 +0200

    Initial Commit

---
+++ MPI/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MPI
+//
+//  Created by Maxence on 04/06/2018.
+//  Copyright © 2018 Maxence. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
