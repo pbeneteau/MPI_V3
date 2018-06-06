2d2edaeb98dbad4f369fa29b2ec5d353bfd1369a
Maxence Guegnolle Santi <maxence@MacBook-Pro-de-Maxence.local>
2018-06-06 23:04:23 +0200

    oui

--- MPI/main.cpp
+++ MPI/main.cpp
@@ -11,28 +11,77 @@ using namespace std;
 int main()
 {
 	string chemin;
+	string Anumber;
+	int choix;
+	
 	Automate    AF;
 	
 	cout << "Siaisr le numero du fichier : ";
-	chemin = "/Users/paulbeneteau/Documents/automates_copy/#05.txt";
-
+	cin >> Anumber;
+	chemin = "#" + Anumber + ".txt";
+	//chemin = "/Users/maxence/Documents/Prog/EFREI/MPI/MPI/MPI/automates/#03.txt";
+	
 	AF.lecture_fichier(chemin);
 	cout << "Lecture du fichier : OK !" << endl;
 	
-	AF.afficher();
-  /*
-    if (AF.est_un_automate_deterministe()) { cout << "Deterministe" << endl; } else {
-        cout << "Non deterministe" << endl;
-    }
-    if (AF.est_un_automate_asynchrone()) { cout << "Asynchrone" << endl; } else { cout << "Synchrone" << endl; }
-    if (AF.est_un_automate_complet()) { cout << "Complet" << endl; } else {
-        cout << "Non complet" << endl;
-    }
-    
-    AF.determination_et_completion();
-	*/
-//	AF.standardiser_automate();
- //   AF.afficher();
-	AF.langage_compl();
-	AF.afficher();
+	do {
+		cout << "Choix d'action :" << endl;
+		cout << "1 : Afficher AF" << endl;
+		cout << "2 : Tester si AF est synchrone" << endl;
+		cout << "3 : Déterminisation et complétion AF synchrone" << endl;
+		cout << "4 : Tester si AF est déterministe" << endl;
+		cout << "5 : Tester si AF est déterministe complet" << endl;
+		cout << "6 : Complétion d’un AF déterministe" << endl;
+		cout << "7 : Production de l’AFDC pour le langage complémentaire" << endl;
+		cout << "8 : Standardisation" << endl;
+		cout << "9 : Quitter" << endl;
+		
+		cout << "Numero du choix ?" << endl;
+		cin >> choix;
+		
+		if (choix == 1) {
+			AF.afficher();
+		}
+		else if (choix ==  2) {
+			if (AF.est_un_automate_asynchrone())
+				cout << "Cet automate est asynchrone" << endl;
+			else
+				cout << "Cet automate n'est pas asynchrone" << endl;
+		}
+		else if (choix == 3) {
+			if (AF.est_un_automate_complet())
+				cout << "Cet automate est complet" << endl;
+			else
+				cout << "Cet automate n'est pas complet" << endl;
+		}
+		else if (choix == 4) {
+			if (AF.est_un_automate_deterministe())
+				cout << "Cet automate est deterministe" << endl;
+			else
+				cout << "Cet automate n'est pas deterministe" << endl;
+		}
+		else if (choix == 5) {
+			if (AF.est_un_automate_deterministe())
+				cout << "Cet automate est deterministe" << endl;
+			else
+				cout << "Cet automate n'est pas deterministe" << endl;
+			if (AF.est_un_automate_complet())
+				cout << "Cet automate est complet" << endl;
+			else
+				cout << "Cet automate n'est pas complet" << endl;
+		}
+		else if (choix == 6) {
+			AF.completion();
+		}
+		else if (choix == 7) {
+			AF.langage_compl();
+		}
+		else if (choix == 8) {
+			AF.standardiser_automate();
+		}
+		else if (choix == 9) {
+			exit(1);
+		}
+		
+	} while (1);
 }
