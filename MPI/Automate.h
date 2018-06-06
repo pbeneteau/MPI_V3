19385be6705ce5a45ed182dcce7b89f04bd6979b
Paul <paulbeneteau@hotmail.fr>
2018-06-06 15:10:49 +0200

    sdfghj

--- MPI/Automate.h
+++ MPI/Automate.h
@@ -60,6 +60,7 @@ class Automate
 	void			handle_initiaux(string ligne);
 	void			handle_terminaux(string ligne);
     string          handle_comma(string &line);
+    int             trouver_index_etat(string etat);
 	
 };
 
